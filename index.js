b0607da27b87eb65d1130e04c7c598a6d1b6718c
syousef2 <156629937+syousef2@users.noreply.github.com>
2025-04-09 01:01:25 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
+function showLogin() {
+  document.getElementById("login-form").syle.display = "block";
+  document.getElementById("signup-form").style.display = "none";
+}
+function showSignup() { 
+  document.getElementById("signup-form").syle.display = "block";
+  document.getElementById("login-form").style.display = "none";
+}
+
+window.onload = showLogin;
 
